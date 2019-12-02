bf4d6d551fb89d5913f668210d3bfaa0a42a6dca
Yamate Kyosuke <yamate@poslnoMacBook-Pro.local>
2019-12-02 17:30:39 +0900

    Improve processor.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import processor
 import exporter
 import argparse
 
+#コマンドライン引数の定義
 parser = argparse.ArgumentParser(description = '')
 
 parser.add_argument('-f', default = './input/pig.png') #ファイルパス
@@ -11,6 +12,7 @@ parser.add_argument('-m', default = 0) #モード
 
 args = parser.parse_args()
 
+
 img = importer.importing(args.f)
-new_img = processor.red_max(img)
+new_img = processor.make_new_img(img, processor.red_max)
 exporter.exporting(new_img,'./output/output.png')
